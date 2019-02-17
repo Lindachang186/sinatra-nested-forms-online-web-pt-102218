864c944ffefe28b918bb05fccd404c047c5119ca
Linda Chang <lindachang186@gmail.com>
2019-02-17 21:50:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,6 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-        binding.pry
         params[:pirate][:ships].each do |details|
         ship = Ship.new(details)
         ship.pirate = @pirate
